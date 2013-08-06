82eb93d858d2fa72cbb6794b039947a0a8e249a4
Edan Schwartz <eschwartz@weathernation.net>
2013-08-06 11:27:54 -0500

    AddWaypointCommand accept an 'at' options to insert a waypoint into a route at a specified index
    - Move common directions service logic
      to Abstract
    
    - Update Example Route app with AddWaypoint(at)Command functionality

--- tests/spec/aeris/maps/gmaps/route/commands/addwaypointcommand.js
+++ tests/spec/aeris/maps/gmaps/route/commands/addwaypointcommand.js
@@ -1,108 +1,367 @@
 define([
   'aeris',
-  'testErrors/untestedspecerror',
-  'testUtils',
   'aeris/promise',
-  'mocks/waypoint',
-  'gmaps/route/route',
   'gmaps/route/commands/addwaypointcommand',
-  'mocks/directionsresults'
-], function(aeris, UntestedSpecError, testUtils, Promise, MockWaypoint, Route, AddWaypointCommand, MockDirectionsResult) {
+  'gmaps/route/waypoint',
+  'gmaps/route/route',
+  'mocks/promise',
+  'mocks/directionsresults',
+  'testUtils',
+  'gmaps/utils',
+  'vendor/underscore',
+  'testErrors/untestedspecerror',
+  'aeris/errors/invalidargumenterror',
+  'sinon'
+], function(
+  aeris,
+  Promise,
+  AddWaypointCommand,
+  Waypoint,
+  Route,
+  StubbedPromise,
+  MockDirectionsResults,
+  testUtils,
+  mapUtils,
+  _,
+  UntestedSpecError,
+  InvalidArgumentError,
+  sinon
+) {
   describe('An AddWaypointCommand', function() {
-    var directionsResult;
 
-    beforeEach(function() {
-      directionsResult = new MockDirectionsResult();
-      spyOn(google.maps.DirectionsService.prototype, 'route').andCallFake(function(request, callback) {
-        callback(directionsResult, google.maps.DirectionsStatus.OK);
-      });
-    });
+    /**
+     * Create a stubbed instance of a {aeris.maps.gmaps.route.Waypoint}.
+     *
+     * @return {aeris.maps.gmaps.route.Waypoint}
+     */
+    function getStubbedWaypoint(opt_options) {
+      var waypoint = sinon.createStubInstance(Waypoint);
+      var pathData;
+      var options = _.extend({
+        followDirections: true
+      }, opt_options);
+
+      waypoint.followDirections = options.followDirections;
+
+      // Mock the fetchPathTo promise
+      // to return empty an promise
+      // This can be overwritten to provide fake data by calling
+      //    args.newWaypoint.fetchPathTo.andCallFake(function() {
+      //      return new StubbedPromise({
+      //        resolve: true,
+      //        args: [{ path: somePath, distance: 12345}]
+      //      });
+      //    });
+
+      // But it will keep tests that don't deal with this command
+      // from breaking.
+      pathData = {
+        path: [],
+        distance: null
+      };
+      spyOn(waypoint, 'fetchPathTo').andReturn(new StubbedPromise({
+        resolve: true,
+        args: [pathData]
+      }));
+
+      return waypoint;
+    }
+
+    function getStubbedRoute() {
+      return sinon.createStubInstance(Route);
+    }
 
 
-    it('adds a waypoint to a route', function() {
-      var route = new Route();
-      var waypoint = new MockWaypoint();
+    /**
+     * Get command mock objects and spies
+     * needed for testing the AddWaypointCommand.
+     * Cuts down on the amount of boilerplate code
+     * needed for each tests
+     *
+     * @param {Object=} opt_options
+     * @param {number=} opt_options.at Index of mocked nextWaypoint.
+     * @param {Boolean=} opt_options.isFirst Is the new waypoint the first in route?
+     * @param {Boolean=} opt_options.isLast Is the new waypoint the last in route?
+     * @param {Boolean=} opt_options.followDirections
+     *
+     *
+     * @return {{
+     *  route: aeris.maps.gmaps.route.Route,
+     *  newWaypoint: aeris.maps.gmaps.route.Waypoint,
+     *  nextWaypoint: aeris.maps.gmaps.route.Waypoint,
+     *  command: gmaps.route.commands.AddWaypointCommand
+     * }}
+     */
+    function getTestObjects(opt_options) {
+      var options = _.extend({
+        followDirections: true,
+        isFirst: opt_options && opt_options.at === 0,
+        isLast: false
+      }, opt_options);
+      var route = getStubbedRoute();
+      var newWaypoint = getStubbedWaypoint({
+                          followDirections: options.followDirections
+                        });
+      var nextWaypoint = options.isLast ?
+                          undefined :
+                          getStubbedWaypoint({
+                            followDirections: options.followDirections
+                          });
+      var prevWaypoint = options.isFirst ?
+                          undefined :
+                          getStubbedWaypoint();
       var command;
 
-      spyOn(route, 'add');
+      // Stub route to pass 'has waypoint' test
+      // For next / prev waypoints
+      spyOn(route, 'has').andCallFake(function(waypoint) {
+        if (_.isUndefined(waypoint)) {
+          return false;
+        }
 
-      command = new AddWaypointCommand(route, waypoint);
-      command.execute();
-
-      expect(route.add).toHaveBeenCalledWith(waypoint);
-    });
-
-    it('should return a promise', function() {
-      var command = new AddWaypointCommand(new Route(), new MockWaypoint());
-      expect(command.execute()).toBeInstanceOf(Promise);
-    });
-
-    it('should resolve its promise after fetching API data', function() {
-      var command = new AddWaypointCommand(new Route(), new MockWaypoint());
-
-      // Service is mocked to immediately call callback
-      command.execute().done(testUtils.setFlag);
-      expect(testUtils.checkFlag()).toEqual(true);
-    });
-
-    describe('Not following paths', function() {
-      it('should set the path property as a direct line to the previous waypoint', function() {
-        var firstWaypoint = new MockWaypoint(null, true);
-        var secondWaypoint = new MockWaypoint({
-          followDirections: false
-        }, true);
-        var route = new Route([firstWaypoint]);
-        var command = new AddWaypointCommand(route, secondWaypoint);
-
-        command.execute().done(function() {
-          expect(route.getLastWaypoint().path).toEqual([
-            firstWaypoint.getLatLon(),
-            secondWaypoint.getLatLon()
-          ]);
-          testUtils.setFlag();
-        });
-
-        waitsFor(testUtils.checkFlag, 'command to execute', 50);
-      });
-    });
-
-    describe('Undo', function() {
-
-      it('should return a promise', function() {
-        var route = new Route();
-        var command = new AddWaypointCommand(route, new MockWaypoint());
-        command.execute().done(testUtils.setFlag);
-
-        waitsFor(testUtils.checkFlag, 'to execute command', 500);
-        runs(function() {
-          expect(command.undo()).toBeInstanceOf(Promise);
-        });
+        return waypoint === nextWaypoint ||
+          waypoint === prevWaypoint;
       });
 
-      it('should fail if command hasn\'t been executed', function() {
-        var command = new AddWaypointCommand(new Route(), new MockWaypoint());
+      // Stub route.getWaypoints to return prev/next waypoints
+      spyOn(route, 'getWaypoints').andCallFake(function() {
+        var waypoints = [];
+        if (prevWaypoint) { waypoints.push(prevWaypoint); }
+        if (nextWaypoint) { waypoints.push(nextWaypoint); }
 
-        expect(function() {
-          command.undo();
-        }).toThrowType('CommandHistoryError');
+        return waypoints;
       });
 
-      it('should revert state', function() {
-        var route = new Route(testUtils.getMockWaypoints());
-        var route_orig = testUtils.cloneRoute(route);
-        var newWaypoint = new MockWaypoint();
-        var command = new AddWaypointCommand(route, newWaypoint);
+      // Stub route to return next / prev waypoints
+      spyOn(route, 'at').andCallFake(function(index) {
+        if (index === options.at) {
+          return nextWaypoint;
+        }
+        else if (index === (options.at - 1)) {
+          return prevWaypoint;
+        }
+        else {
+          return undefined;
+        }
+      });
 
-        command.execute().done(function() {
-          command.undo().done(testUtils.setFlag);
+      command = new AddWaypointCommand(route, newWaypoint, { at: options.at });
+
+      return {
+        route: route,
+        newWaypoint: newWaypoint,
+        nextWaypoint: nextWaypoint,
+        prevWaypoint: prevWaypoint,
+        command: command
+      };
+    }
+
+    it('should complete execution', function() {
+      var args = getTestObjects();
+      args.command.execute().done(testUtils.setFlag);
+
+      waitsFor(testUtils.checkFlag, 'command to complete execution', 25);
+    });
+
+    describe('Adding a waypoint', function() {
+
+      it('should add the waypoint at the correct index', function() {
+        var nextIndex = 3;
+        var args = getTestObjects({ at: nextIndex });
+
+        spyOn(args.route, 'add');
+
+        // Execute command
+        args.command.execute();
+
+        // Check that waypoint is added
+        // at the index of the next waypoint
+        expect(args.route.add).toHaveBeenCalledWith(args.newWaypoint, { at: nextIndex });
+      });
+
+      describe('before the first waypoint in a route', function() {
+        it('should add a waypoint with no path or distance', function() {
+          var args = getTestObjects({ at: 0 });
+
+          args.command.execute().done(testUtils.setFlag);
+
+          waitsFor(testUtils.checkFlag, 'command to execute', 25);
+          runs(function() {
+            expect(args.newWaypoint.path).toBeFalsy();
+            expect(args.newWaypoint.distance).toBeFalsy();
+            expect(testUtils.getSpies()).toHaveAllBeenCalled();
+          });
+        });
+      });
+
+      describe('should update the path and distance next waypoint', function() {
+
+        it('when the next waypoint is following directions', function() {
+          var directions = {
+            path: testUtils.getRandomPath(),
+            distance: 54321
+          };
+          directions.geocodedLatLon = directions.path[directions.path.length - 1];
+
+          var args = getTestObjects({ at: 0 });
+
+          // Create stub methods,
+          // and add them to our list of "must be called" spies
+          testUtils.addSpies(
+            // Mock directions response
+            args.newWaypoint.fetchPathTo.andCallFake(function(waypoint) {
+              expect(waypoint).toEqual(args.nextWaypoint);
+
+              return new StubbedPromise({
+                resolve: true,
+                args: [directions]
+              });
+            }),
+
+            // Test: nextWaypoint is updated with path data
+            spyOn(args.nextWaypoint, 'set').andCallFake(function(attrs) {
+              expect(attrs).toEqual(directions);
+            })
+          );
+
+
+          // Execute the command, already
+          args.command.execute().done(testUtils.setFlag);
+
+          waitsFor(testUtils.checkFlag, 'command to execute', 25);
+          runs(function() {
+            expect(testUtils.getSpies()).toHaveAllBeenCalled();
+          });
         });
 
-        waitsFor(testUtils.checkFlag, 'to undo command', 500);
-        runs(function() {
-          expect(route.getWaypoints().length).toEqual(3);
-          expect(route).toMatchRoute(route_orig);
+        it('when the next waypoint is not following directions', function() {
+          var args = getTestObjects({
+            at: 0,
+            followDirections: false
+          });
+          var resPath = testUtils.getRandomPath(2);
+          var distance = 12345;
+
+          // Mock straight-line path getters
+          testUtils.addSpies(
+            spyOn(args.newWaypoint, 'getDirectPathTo').andCallFake(function(waypoint) {
+              expect(waypoint).toEqual(args.nextWaypoint);
+              return resPath;
+            }),
+
+            spyOn(args.newWaypoint, 'calculateDirectDistanceTo').andCallFake(function(waypoint) {
+              expect(waypoint).toEqual(args.nextWaypoint);
+              return distance;
+            }),
+
+            // Test: nextWaypoint is updated with path data
+            spyOn(args.nextWaypoint, 'set').andCallFake(function(attrs) {
+              expect(attrs).toEqual({
+                path: resPath,
+                distance: distance
+              });
+            })
+          );
+
+          // Run, Forest, run
+          args.command.execute().done(testUtils.setFlag);
+
+          waitsFor(testUtils.checkFlag, 'command to execute', 25);
+          runs(function() {
+            expect(testUtils.getSpies()).toHaveAllBeenCalled();
+          });
+        });
+      });
+
+      describe('should create a path and distance to the previous waypoint', function() {
+        it('when the new waypoint is following directions', function() {
+          var atIndex = 2;
+          var path = testUtils.getRandomPath();
+          var directions = {
+            path: path,
+            distance: 12345
+          };
+          var args = getTestObjects({ at: atIndex });
+
+          testUtils.addSpies(
+            // Provide path data
+            args.prevWaypoint.fetchPathTo.andCallFake(function(waypoint) {
+              expect(waypoint).toEqual(args.newWaypoint);
+
+              return new StubbedPromise({
+                resolve: true,
+                args: [directions]
+              });
+            }),
+
+            // Test: path data is set on newWaypoint
+            spyOn(args.newWaypoint, 'set')
+          );
+
+          args.command.execute().done(testUtils.setFlag);
+
+          waitsFor(testUtils.checkFlag, 'command to execute', 25);
+          runs(function() {
+            expect(testUtils.getSpies()).toHaveAllBeenCalled();
+            expect(args.newWaypoint.set).toHaveBeenCalledWith(_.extend(directions, {
+              geocodedLatLon: directions.path[directions.path.length - 1]
+            }));
+          });
+        });
+
+        it('when the new waypoint is not following directions', function() {
+          var atIndex = 2;
+          var args = getTestObjects({
+            at: atIndex,
+            followDirections: false
+          });
+          var directions = {
+            path: testUtils.getRandomPath(2),
+            distance: 12345
+          };
+
+          testUtils.addSpies(
+            // Provide direct path
+            spyOn(args.prevWaypoint, 'getDirectPathTo').andCallFake(function(waypoint) {
+              expect(waypoint).toEqual(args.newWaypoint);
+              return directions.path;
+            }),
+
+            // Provide directions
+            spyOn(args.prevWaypoint, 'calculateDirectDistanceTo').andCallFake(function(waypoint) {
+              expect(waypoint).toEqual(args.newWaypoint);
+              return directions.distance;
+            }),
+
+            // Test: set directions attribute on newWaypoint
+            spyOn(args.newWaypoint, 'set').andCallFake(function(attrs) {
+              expect(attrs).toEqual(directions);
+            })
+          );
+
+          args.command.execute().done(testUtils.setFlag);
+
+          waitsFor(testUtils.checkFlag, 'command to execute', 25);
+          runs(function() {
+            expect(testUtils.getSpies()).toHaveAllBeenCalled();
+          });
+        });
+      });
+
+
+      describe('at the end of the route', function() {
+        it('should not run into any unexpected issues', function() {
+          // All the logic's been tested
+          // We'll just make sure we're not throwing any errors
+          var args = getTestObjects({
+            at: 3,
+            isLast: true
+          });
+
+          args.command.execute();
         });
       });
     });
   });
 });
+
