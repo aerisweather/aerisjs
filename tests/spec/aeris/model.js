9c2b7e5dee899547340e6acbd2d7092667633baa
Edan Schwartz <eschwartz@weathernation.net>
2013-10-30 12:07:38 -0500

    Model: normalize attributes before setting

--- tests/spec/aeris/model.js
+++ tests/spec/aeris/model.js
@@ -56,5 +56,59 @@ define([
         test.model.isValid();
       });
     });
+
+
+    describe('set', function() {
+
+      it('should normalize attribute before setting', function() {
+        var model = new Model();
+        var rawAttrs = { foo: 'bar', yo: 'wazaam' };
+
+        model.normalize_ = function(attrs) {
+          _.each(attrs, function(value, key) {
+            attrs[key] = value + '_normal';
+          });
+
+          return attrs;
+        };
+
+        model.set(rawAttrs);
+
+        expect(model.get('foo')).toEqual('bar_normal');
+        expect(model.get('yo')).toEqual('wazaam_normal');
+      });
+
+      // Spec adapted from Backbone.Model spec
+      // https://github.com/jashkenas/backbone/blob/master/test/model.js ~line 202
+      it('should act like normal', function() {
+        var a = new Model({id: 'id', foo: 1, bar: 2, baz: 3});
+        var changeCount = 0;
+
+        a.on('change:foo', function() { changeCount += 1; });
+        a.set({'foo': 2});
+
+        expect(a.get('foo')).toEqual(2);
+        expect(changeCount).toEqual(1);
+
+        a.set({'foo': 2}); // set with value that is not new shouldn't fire change event
+        expect(a.get('foo')).toEqual(2);  // Foo should NOT have changed, still 2
+        expect(changeCount).toEqual(1);   // Change count should NOT have incremented.
+
+        a.validate = function(attrs) {
+          expect(attrs.foo).toEqual(void 0);  // validate:true passed while unsetting
+        };
+
+        a.unset('foo', {validate: true});
+        expect(a.get('foo')).toEqual(void 0); // Foo should have changed
+
+        delete a.validate;
+        expect(changeCount).toEqual(2);       // 'Change count should have incremented for unset.
+
+        a.unset('id');
+        expect(a.id).toBeUndefined();         // Unsetting the id should remove the id property.
+      });
+
+    });
+
   });
 });
