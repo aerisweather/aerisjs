b515cf82cd76923a87dc4f7db5e2376b73e55eb4
Edan Schwartz <eschwartz@weathernation.net>
2013-12-02 16:24:13 -0600

    Item/LayoutControllers: Mixin ViewMixin

--- tests/SpecRunner.js
+++ tests/SpecRunner.js
@@ -37,6 +37,7 @@ require([
       //'spec/aeris/datehelper',
       //'spec/aeris/classfactory',
 
+      //'spec/aeris/application/controller/mixin/viewmixin',
       //'spec/aeris/application/model/eventparambag',
       //'spec/aeris/application/module/module',
       //'spec/aeris/application/menu/controller/menucontroller',
