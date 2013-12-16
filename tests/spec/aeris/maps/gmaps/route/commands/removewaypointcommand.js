0732d01739b6619aeffc31716ae1c3bb9011adcb
Edan Schwartz <eschwartz@weathernation.net>
2013-12-16 10:12:45 -0600

    RemoveWaypointCommand: Fix failing spec, clean up

--- tests/spec/aeris/maps/gmaps/route/commands/removewaypointcommand.js
+++ tests/spec/aeris/maps/gmaps/route/commands/removewaypointcommand.js
@@ -1,70 +1,108 @@
 define([
-  'jasmine',
-  'sinon',
   'aeris/util',
   'gmaps/route/commands/removewaypointcommand',
   'gmaps/route/route',
-  'gmaps/route/waypoint'
-], function(
-  jasmine,
-  sinon,
-  _,
-  RemoveWaypointCommand,
-  Route,
-  Waypoint
-) {
-  function getStubbedWaypoint() {
-    return sinon.createStubInstance(Waypoint);
-  }
-
-  function getStubbedRoute(at_index) {
-    var route = sinon.createStubInstance(Route);
-    route.remove = jasmine.createSpy('remove');
-    route.add = jasmine.createSpy('add');
-    route.indexOf = jasmine.createSpy('indexOf').andReturn(at_index);
-    return route;
-  }
-
-  var TestFactory = function(opt_at_index) {
-    var at_index = _.isUndefined(opt_at_index) ? 0 : opt_at_index;
-    var self = this;
-
-    this.waypoint_ = getStubbedWaypoint();
-    this.route_ = getStubbedRoute(at_index);
-
-    this.getRoute().has = jasmine.createSpy('has').andCallFake(function(waypoint) {
-      expect(waypoint).toEqual(self.getWaypoint());
-      return true;
-    });
-
-    this.command_ = new RemoveWaypointCommand(this.getRoute(), this.getWaypoint());
+  'aeris/model',
+  'aeris/collection',
+  'aeris/promise'
+], function(_, RemoveWaypointCommand, Route, Model, Collection, Promise) {
+  var MockWaypoint = function() {
+    Model.apply(this, arguments);
   };
-  TestFactory.prototype = {
-    getRoute: function() { return this.route_; },
-    getWaypoint: function() { return this.waypoint_; },
-    getCommand: function() { return this.command_; }
+  _.inherits(MockWaypoint, Model);
+
+  var MockRoute = function() {
+    Collection.apply(this, arguments);
   };
+  _.inherits(MockRoute, Route);
 
 
   describe('A RemoveWaypointCommand', function() {
-    it('should remove a waypoint', function() {
-      var f = new TestFactory();
 
-      f.getCommand().execute();
-      expect(f.getRoute().remove).toHaveBeenCalledWith(f.getWaypoint());
+
+    describe('execute', function() {
+      var route, command, waypointToRemove;
+
+
+      beforeEach(function() {
+        route = new MockRoute();
+        waypointToRemove = new MockWaypoint();
+        route.add(waypointToRemove);
+
+        command = new RemoveWaypointCommand(route, waypointToRemove);
+
+        spyOn(route, 'remove');
+      });
+
+
+      it('should return a promise', function() {
+        expect(command.execute()).toBeInstanceOf(Promise);
+      });
+
+      it('should remove the waypoint from the route', function() {
+        command.execute();
+
+        expect(route.remove).toHaveBeenCalledWith(waypointToRemove);
+      });
+
+      it('should resolve the promise immediately', function() {
+        var promiseToExecute = command.execute();
+
+        expect(promiseToExecute.getState()).toEqual('resolved');
+      });
+
+      it('should throw an error if the waypoint is not in the route', function() {
+        var waypointNotInRoute = new MockWaypoint();
+        var command = new RemoveWaypointCommand(route, waypointNotInRoute);
+
+        expect(function() {
+          command.execute();
+        }).toThrowType('WaypointNotInRouteError');
+      });
+
     });
 
-    it('should undo', function() {
-      var wpIndex = 2;
-      var f = new TestFactory(wpIndex);
 
-      f.getCommand().execute();
-      f.getCommand().undo();
+    describe('undo', function() {
+      var route, removedWaypoint, REMOVED_AT;
+      var command;
 
-      expect(f.getRoute().add).toHaveBeenCalledWith(
-        f.getWaypoint(),
-        { at: wpIndex }
-      );
+      beforeEach(function() {
+        REMOVED_AT = 1;
+        route = new MockRoute([new MockWaypoint(), new MockWaypoint()])
+        removedWaypoint = new MockWaypoint();
+
+        route.add(removedWaypoint, { at: REMOVED_AT });
+
+        command = new RemoveWaypointCommand(route, removedWaypoint);
+
+        command.execute();
+      });
+
+
+      it('should return a promise', function() {
+        expect(command.undo()).toBeInstanceOf(Promise);
+      });
+
+      it('should add the waypoint back to the route', function() {
+        command.undo();
+
+        expect(route.contains(removedWaypoint)).toEqual(true);
+      });
+
+      it('should add the waypoint back in the same index at which it was added', function() {
+        command.undo();
+
+        expect(route.indexOf(removedWaypoint)).toEqual(REMOVED_AT);
+      });
+
+      it('should resolve its promise immediately', function() {
+        var promiseToUndo = command.undo();
+
+        expect(promiseToUndo.getState()).toEqual('resolved');
+      });
+
     });
+
   });
 });
