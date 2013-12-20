4c281c9db94cc5867ef07d29df58e2d3687ab77f
Edan Schwartz <eschwartz@weathernation.net>
2013-12-20 11:30:53 -0600

    Deployment: Google Maps build package

---
+++ deployment/packages/gmaps/gmaps.js
@@ -0,0 +1,46 @@
+({
+  name: 'base/map',
+  out: '../../../build/packages/gmaps.js',
+
+  mainConfigFile: '../../../lib/config.js',
+  baseUrl: '../../../lib',
+
+  map: {
+    '*': {
+      strategy: 'gmaps'
+    }
+  },
+
+  paths: {
+    //vendor: 'empty:',
+    'vendor/underscore': 'empty:',
+    'vendor/backbone': 'empty:',
+    'vendor/marionette': 'empty:',
+    'vendor/jquery': 'empty:',
+    'vendor/handlebars': 'empty:',
+
+    // For an example of using
+    // wire with r.js, see:
+    // https://github.com/pieter-vanderwerff/backbone-require-wire
+    'wire/build/amd/builder': 'vendor/wire/rjs/builder'
+  },
+  optimize: 'none',
+  exclude: ['vendor/text'],
+  include: [
+    'vendor/libs',
+    'vendor/config',
+
+    'packages/animations',
+    'packages/infobox',
+    'packages/layers',
+    'packages/markers',
+    'packages/gmaps',
+
+    'aeris/loader/loader',
+    'aeris/maps/loader'
+  ],
+  wrap: {
+    startFile: ['../../../externals/require.js', '../../start.frag.js'],
+    endFile: ['../../end.frag.js']
+  }
+})
