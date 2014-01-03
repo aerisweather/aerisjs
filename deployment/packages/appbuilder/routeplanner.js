bd8e1fbc644c2ac5fc70cfe815417c9662743ecd
Edan Schwartz <eschwartz@weathernation.net>
2014-01-03 16:10:57 -0600

    Polaris RoutePlanner Build config + example html

---
+++ deployment/packages/appbuilder/routeplanner.js
@@ -0,0 +1,47 @@
+({
+  name: 'polaris/routeplanner/routeplannerbuilder',
+  out: '../../../build/packages/routeplanner.js',
+
+  mainConfigFile: '../../../lib/config.js',
+  baseUrl: '../../../lib',
+
+  paths: {
+    'vendor/underscore': 'empty:',
+    'vendor/backbone': 'empty:',
+    'vendor/marionette': 'empty:',
+    'vendor/jquery': 'empty:',
+
+    strategy: 'aeris/maps/gmaps'
+  },
+
+  optimize: 'none',
+  preserveLicenseComments: false,
+
+  // Handlebars config
+  inlineText: true,
+  stubModules: ['text', 'hbars'],
+  onBuildWrite : function(moduleName, path, content){
+    // replace handlebars with the runtime version
+    if (moduleName === 'Handlebars') {
+      path = path.replace('handlebars.js','handlebars.runtime.js');
+      content = fs.readFileSync(path).toString();
+      content = content.replace(/(define\()(function)/, '$1"handlebars", $2');
+    }
+    return content;
+  },
+
+  include: [
+    'vendor/config',
+
+    'packages/maps',
+    'packages/gmaps',
+    'polaris/packages/routeplanner',
+
+    'aeris/loader/loader',
+    'polaris/routeplanner/loader'
+  ],
+  wrap: {
+    startFile: ['../../../externals/require.js', '../../start.frag.js'],
+    endFile: ['../../end.frag.js']
+  }
+})
