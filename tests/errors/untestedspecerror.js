4435a7e8ab3ee0ddd3eaf05323d7af17efcb5019
Edan Schwartz <eschwartz@weathernation.net>
2014-01-09 11:19:06 -0600

    Use ErrorTypeFactory for all Error class definitions

--- tests/errors/untestedspecerror.js
+++ tests/errors/untestedspecerror.js
@@ -1,38 +1,14 @@
-/**
- * @fileoverview Defines an UnimplementedSpecError.
-*/
-define(['aeris/util', 'aeris/errors/abstracterror'], function(_, AbstractError) {
+define([
+  'aeris/errors/errortypefactory'
+], function(ErrorTypeFactory) {
   /**
-   * UnimplementedSpecError
-   *
-   * Use when a functionality has a spec defined,
-   * but with not actual test.
-   *
-   * Useful for creating test skeletons.
-   * eg.
-   *
-   * it('should do something or other', function() {
-   *   throw new UntestedSpecError();
-   * });
-   *
-   * @constructor
-   */
-  var UntestedSpecError = function() {
-    AbstractError.apply(this, arguments);
-  };
-
-  _.inherits(
-    UntestedSpecError,
-    AbstractError
-  );
-
-  UntestedSpecError.prototype.setName = function() {
-    return 'UntestedSpecError';
-  };
-
-  UntestedSpecError.prototype.setMessage = function(message) {
-    return message || 'Spec has not yet been tested';
-  };
-
-  return UntestedSpecError;
+   * @class UntestedSpecError
+   * @extends aeris.errors.AbstractError
+  */
+  return new ErrorTypeFactory({
+    name: 'UntestedSpecError',
+    message: function(message) {
+      return message || 'Spec has not yet been tested';
+    }
+  });
 });
