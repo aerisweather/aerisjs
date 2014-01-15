c6440b555b86c22dd4667ebbfcfa2cabd9ed923e
Edan Schwartz <eschwartz@weathernation.net>
2014-01-15 12:04:17 -0600

    Move Bower components to base directory

--- tests/testConfig.js
+++ tests/testConfig.js
@@ -3,11 +3,11 @@ require.config({
   //urlArgs: 'cb=' + Math.random(),                   // Cache buster
   paths: {
     tests: '../tests',
-    jasmine: 'vendor/bower_components/jasmine/lib/jasmine-core/jasmine',
-    'jasmine-slow': 'vendor/bower_components/jasmine-slow/lib/jasmine-slow',
+    jasmine: '../bower_components/jasmine/lib/jasmine-core/jasmine',
+    'jasmine-slow': '../bower_components/jasmine-slow/lib/jasmine-slow',
     'jasmine-html': '/tests/lib/jasmine-html',
     async: '/tests/lib/async',
-    sinon: 'vendor/bower_components/sinon/index',
+    sinon: '../bower_components/sinon/index',
     spec: '/tests/spec',
     mocks: '/tests/mocks',
     matchers: '/tests/lib/matchers',
