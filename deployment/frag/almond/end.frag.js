34c4c1448077310b4556baf7420558a8c63b78ec
Edan Schwartz <eschwartz@weathernation.net>
2014-01-20 17:11:16 -0600

    Deployment: Fix for loading providing entire pacakge in AlmondJS build

--- deployment/frag/almond/end.frag.js
+++ deployment/frag/almond/end.frag.js
@@ -1,12 +1,5 @@
   // In order for modules to be immediately available
-  // under the aeris namespace, they must be required here.
-  // Using commonJS format with almond --> synchronous require.
-  //
-  // To make this sustainable, we will need a build script
-  // with writes these require statements from a package
-  // config file.
-  require('ai/maps/map');
-  require('ai/maps/marker');
-  require('ai/maps/infobox');
+  // under the aeris namespace, packages must be required here.
+  require('ai/packages/maps');
   return aeris;
 }));
