2af4cd2eb27036ebe8606479497a463060e3e232
Edan Schwartz <eschwartz@weathernation.net>
2014-02-07 13:38:59 -0600

    Register public theme partials with API theme docs

--- docs/themes/api/helpers/helpers.js
+++ docs/themes/api/helpers/helpers.js
@@ -1,6 +1,25 @@
 (function(module) {
   var _ = require('underscore');
   var createNamespaceTree = require('../../../scripts/createnamespacetree');
+  var handlebars = require('yuidocjs').Handlebars;
+  var fs = require('fs');
+  var path = require('path');
+
+
+  function registerPartialsInDir(partialsDir, prefix) {
+    var partialPaths = fs.readdirSync(partialsDir);
+
+    partialPaths.forEach(function(fileName) {
+      var html = fs.readFileSync(path.join(partialsDir, fileName), 'utf8');
+      var partialName = path.basename(fileName, '.handlebars');
+
+      handlebars.registerPartial(prefix + '/' + partialName, html);
+    });
+  }
+
+  // Register public theme partials
+  // under 'public/' prefix.
+  registerPartialsInDir('themes/public/partials', 'public');
 
   var helpers = {
     eachNamespace: function(classes, options) {
