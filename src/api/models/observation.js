fe24cab62798a8f4ff6e0c68d9f6a750d5a348f5
chicks <chicks@weathernation.net>
2014-02-24 11:27:10 -0600

    Updated observation endpoint model to extend from AerisApiModel.

--- src/api/models/observation.js
+++ src/api/models/observation.js
@@ -6,30 +6,19 @@ define([
      * @publicApi
      * @class Observation
      * @namespace aeris.api.model
-     * @extends aeris.api.endpoint.model.PointData
+     * @extends aeris.api.endpoint.model.AerisApiModel
      *
      * @constructor
      * @override
      */
     var Observation = function(opt_attrs, opt_options) {
-        PointData.call(this, opt_attrs, opt_options);
+        var options = _.defaults(opt_options || {}, {
+            endpoint: 'observations'
+        });
+
+        AerisApiModel.call(this, opt_attrs, options);
     };
-    _.inherits(Observation, PointData);
-
-    /**
-     * @method parse
-     */
-    Observation.prototype.parse = function(res) {
-        var attrs = PointData.prototype.parse.apply(this, arguments);
-
-        if (!res.report || !res.report.id) {
-            throw new ApiResponseError('Missing id');
-        }
-
-        attrs.id = res.report.id;
-
-        return attrs;
-    };
+    _.inherits(Observation, AerisApiModel);
 
     return _.expose(Observation, 'aeris.api.Observation');
 });
