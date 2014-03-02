b8ff74c31dde2118c03e8a3a3fe824fe9fb742a3
Edan Schwartz <edan@edanschwartz.com>
2014-03-02 14:58:53 -0600

    Test: fix async AMD path to use bower component

--- tests/testConfig.js
+++ tests/testConfig.js
@@ -6,7 +6,6 @@ require.config({
     jasmine: '../bower_components/jasmine/lib/jasmine-core/jasmine',
     'jasmine-html': '../bower_components/jasmine/lib/jasmine-core/jasmine-html',
     'jasmine-slow': '../bower_components/jasmine-slow/lib/jasmine-slow',
-    async: 'tests/lib/async',
     sinon: '../bower_components/sinon/index',
     spec: 'tests/spec',
     integration: 'tests/integration',
