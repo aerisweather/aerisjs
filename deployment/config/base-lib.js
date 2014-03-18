0155f318b5fe95b1eae79f4d5ed4ea649c1104e6
Edan Schwartz <eschwartz@weathernation.net>
2014-03-18 13:26:59 -0500

    r.js config builder script

---
+++ deployment/config/base-lib.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+
+module.exports = {
+  name: 'almond',
+
+  mainConfigFile: 'config-amd.js',
+
+  paths: {
+    'aeris/maps/strategy': 'src/maps/gmaps'
+  },
+
+  optimize: 'none',
+
+  preserveLicenseComments: false,
+
+  // Handlebars config
+  inlineText: true,
+  stubModules: [
+    // Text and hbars AMD plugin
+    // build text files into optimized package
+    'text', 'hbars',
+
+    // jQuery is not required for
+    // core aeris map library
+    // (need to be stubbed, because Backbone thinks
+    //  it needs it)
+    'jquery'
+  ],
+  onBuildWrite: function(moduleName, path, content) {
+    // replace handlebars with the runtime version
+    if (moduleName === 'Handlebars') {
+      path = path.replace('handlebars.js', 'handlebars.runtime.js');
+      content = fs.readFileSync(path).toString();
+      content = content.replace(/(define\()(function)/, '$1"handlebars", $2');
+    }
+    return content;
+  }
+}
