02eb7b1594736343319ee771445286bbc792b05e
Edan Schwartz <eschwartz@weathernation.net>
2014-03-20 11:51:49 -0500

    Update grunt jasmine-amd config to facilitate multiple test targets

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,35 +11,44 @@ module.exports = function(grunt) {
     },
     pkg: grunt.file.readJSON('package.json'),
     'jasmine-amd': {
-      aeris: {
+      options: {
+        amdConfig: [
+          '../config-amd',
+          'testconfig'
+        ],
+
+        libs: [
+          'jasmine',
+          'jasmine-console',
+          'jasmine-html',
+          'matchers/matchers.package'
+        ],
+        exclude: [
+          // Non-specs
+          'tests/spec/**/mocks/*.js',
+          'tests/spec/**/context.js',
+          'tests/spec/**/fixtures/*.js',
+          'tests/spec/**/config/**/*.js',
+          'tests/spec/integration/helpers/**/*.js',
+
+          // Failing tests
+          'tests/spec/aeris/commands/abstractcommand.js',
+          'tests/spec/aeris/commands/commandmanager.js',
+          'tests/spec/aeris/builder/maps/options/mapappbuilderoptions.js',
+          'tests/spec/aeris/builder/maps/map/controllers/mapcontroller.js'
+        ]
+      },
+      all: {
         options: {
           specs: [
-            'tests/spec/aeris/**/*.js'
-          ],
-          exclude: [
-            // Non-specs
-            'tests/spec/**/mocks/*.js',
-            'tests/spec/**/context.js',
-            'tests/spec/**/fixtures/*.js',
-            'tests/spec/**/config/**/*.js',
-
-            // Failing tests
-            'tests/spec/aeris/commands/abstractcommand.js',
-            'tests/spec/aeris/commands/commandmanager.js',
-            'tests/spec/aeris/builder/maps/options/mapappbuilderoptions.js',
-            'tests/spec/aeris/builder/maps/map/controllers/mapcontroller.js'
-          ],
-
-          amdConfig: [
-            '../config-amd',
-            'testconfig'
-          ],
-
-          libs: [
-            'jasmine',
-            'jasmine-console',
-            'jasmine-html',
-            'matchers/matchers.package'
+            'tests/spec/**/*.js'
+          ]
+        }
+      },
+      integration: {
+        options: {
+          specs: [
+            'tests/spec/integration/**/*.js'
           ]
         }
       }
