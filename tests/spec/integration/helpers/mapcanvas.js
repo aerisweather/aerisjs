e877f107a02153682281f2054f5ab830a0fd2082
Edan Schwartz <eschwartz@weathernation.net>
2014-03-31 14:32:17 -0500

    MapCanvas test helper: use unique element id

--- tests/spec/integration/helpers/mapcanvas.js
+++ tests/spec/integration/helpers/mapcanvas.js
@@ -1,7 +1,9 @@
-define(function() {
+define([
+  'aeris/util'
+], function(_) {
   var MapCanvas = function(opt_id) {
     var mapCanvas = document.createElement('div');
-    var id = opt_id || 'MAP_CANVAS_ID';
+    var id = opt_id || _.uniqueId('MAP_CANVAS_ID_');
     mapCanvas.setAttribute('id', id);
     mapCanvas.style.height = '100px';
 
