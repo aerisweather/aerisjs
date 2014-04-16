0523191718f7cb7c9205b2dd56d96a320f2dba5c
Edan Schwartz <eschwartz@weathernation.net>
2014-04-16 11:53:56 -0500

    minor yuidocs (test lib)

--- tests/spec/integration/helpers/mapcanvas.js
+++ tests/spec/integration/helpers/mapcanvas.js
@@ -1,6 +1,12 @@
 define([
   'aeris/util'
 ], function(_) {
+  /**
+   * @class MockMapCanvas
+   * @param {string} opt_id Element id attribute.
+   * @return {HTMLElement}
+   * @constructor
+   */
   var MapCanvas = function(opt_id) {
     var mapCanvas = document.createElement('div');
     var id = opt_id || _.uniqueId('MAP_CANVAS_ID_');
