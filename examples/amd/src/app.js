9313ec0bc67b550829570345926a941f75713220
Edan Schwartz <eschwartz@weathernation.net>
2014-05-07 09:08:18 -0500

    AMD Example uses Leaflet

--- examples/amd/src/app.js
+++ examples/amd/src/app.js
@@ -1,20 +1,24 @@
 require.config({
   paths: {
     // Base directory of the aerisjs library.
-    aeris: '../bower_components/aerisjs/src',
+    aeris: '../../../src',
 
     // Set the map strategy to use
-    'aeris/maps/strategy': '../bower_components/aerisjs/src/maps/gmaps',            // Use Google Maps
-    // 'aeris/maps/strategy': '../bower_components/aerisjs/src/maps/openlayers',    // Use OpenLayers
+    'aeris/maps/strategy': '../../../src/maps/leaflet',            // Use Leaflet
+    // 'aeris/maps/strategy': '../bower_components/aerisjs/src/maps/gmaps',           // Use Google Maps
 
     underscore: '../bower_components/underscore/underscore',
     jquery: '../bower_components/jquery/dist/jquery',
     backbone: '../bower_components/backbone/backbone',
 
-    googlemaps: '../bower_components/googlemaps-amd/src/googlemaps',
-    async: '../bower_components/requirejs-plugins/src/async',
+    // Required only if using Leaflet
+    leaflet: '//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.2/leaflet',
+    'leaflet-markercluster': '//cdnjs.cloudflare.com/ajax/libs/leaflet.markercluster/0.4.0/leaflet.markercluster'
 
-    'gmaps-markerclusterer-plus': '../bower_components/gmaps-markerclusterer-plus/index'
+    // Required only if using google maps
+    /*googlemaps: '../bower_components/googlemaps-amd/src/googlemaps',
+    async: '../bower_components/requirejs-plugins/src/async',
+    'gmaps-markerclusterer-plus': '../bower_components/gmaps-markerclusterer-plus/index'*/
   },
 
   config: {
@@ -27,8 +31,20 @@ require.config({
   },
 
   shim: {
-    'gmaps-markerclusterer-plus': {
+    // Shim for google maps MarkerClustererPlus
+    // Required only if using google maps
+    /*'gmaps-markerclusterer-plus': {
       exports: 'MarkerClusterer'
+    },*/
+
+    // Shim for Leaflet MarkerCluster
+    // Required only if using Leaflet
+    'leaflet-markercluster': {
+      deps: ['leaflet'],
+      exports: 'L.MarkerClusterGroup',
+      init: function() {
+        return L.MarkerClusterGroup;
+      }
     }
   }
 });
