6283a68ecee97f4f6580bc1e30d194960535f3d7
Edan Schwartz <eschwartz@weathernation.net>
2014-05-14 16:00:40 -0500

    Lightning: set unique id
    - Lightning endpoint does not provide id,
      which was causing collection.fetch() to add duplicate
      models

--- src/api/models/lightning.js
+++ src/api/models/lightning.js
@@ -1,7 +1,8 @@
 define([
   'aeris/util',
-  'aeris/api/models/pointdata'
-], function(_, PointData) {
+  'aeris/api/models/pointdata',
+  'aeris/errors/apiresponseerror'
+], function(_, PointData, ApiResponseError) {
   /**
    * Represents a lightning data response from the AerisApi
    *
@@ -17,6 +18,38 @@ define([
     PointData.call(this, opt_attrs, opt_options);
   };
   _.inherits(Lightning, PointData);
+
+
+  /**
+   * @method parse
+   */
+  Lightning.prototype.parse = function(attrs) {
+    try {
+      attrs.id = this.generateId_(attrs);
+    }
+    catch (e) {
+      if (e instanceof ReferenceError) {
+        throw new ApiResponseError('Unable to determine Lightning id: ' + e.message);
+      }
+    }
+
+    return attrs;
+  };
+
+
+  /**
+   * @method generateId_
+   * @private
+   * @param {Object} attrs Raw response object.
+   * @return {string} Generated id value
+   */
+  Lightning.prototype.generateId_ = function(attrs) {
+    // The lightning endpoint does not provide an id
+    // Create a unique identifier here, so that we
+    // can determine which models from the server
+    // we already have in a collection.
+    return '' + attrs.loc.lat + attrs.loc.long + attrs.obTimestamp;
+  };
 
 
   return _.expose(Lightning, 'aeris.api.models.Lightning');
