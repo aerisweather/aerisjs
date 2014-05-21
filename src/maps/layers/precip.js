61564d8bb701fd20f629c97cb33ab5b67ff5065a
Edan Schwartz <eschwartz@weathernation.net>
2014-05-21 11:10:08 -0500

    Precip tile layer

---
+++ src/maps/layers/precip.js
@@ -0,0 +1,29 @@
+define([
+  'aeris/util',
+  'aeris/maps/layers/aeristile'
+], function(_, AerisTile) {
+  /**
+   * Precipitation layer.
+   *
+   * @class Precip
+   * @namespace aeris.maps.layers
+   * @extends aeris.maps.layers.AerisTile
+   * @publicApi
+   *
+   * @constructor
+   */
+  var Precip = function(opt_attrs, opt_options) {
+    var attrs = _.defaults(opt_attrs || {}, {
+      name: 'Precip',
+      tileType: 'current_precip_1hr',
+      futureTileType: 'frain_hrrr',
+      autoUpdateInterval: AerisTile.updateIntervals.CURRENT
+    });
+
+    AerisTile.call(this, attrs, opt_options);
+  };
+  _.inherits(Precip, AerisTile);
+
+
+  return _.expose(Precip, 'aeris.maps.layers.Precip');
+});
