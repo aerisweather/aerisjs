036b6a08dfff7ead6dc8cd16e513aba66028eb5f
Edan Schwartz <eschwartz@weathernation.net>
2014-06-19 10:06:29 -0500

    MockMap: set default attribute values

--- tests/mocks/aeris/maps/map.js
+++ tests/mocks/aeris/maps/map.js
@@ -9,19 +9,22 @@ define([
    * @constructor
    */
   var MockMap = MockFactory({
+    getSetters: [
+      'bounds',
+      'center',
+      'zoom'
+    ],
     methods: [
-      'getBounds',
       'getView',
-      'setCenter',
-      'getCenter',
-      'setZoom',
-      'setBaseLayer',
       'fitToBounds'
     ],
     name: 'Mock_Map',
     constructor: function(el, opt_attrs, opt_options) {
       var attrs = _.defaults(opt_attrs || {}, {
-        el: el
+        el: el,
+        bounds: [0, 0, 100, 100],
+        center: [0, 0],
+        zoom: 10
       });
 
       MockMapObject.call(this, attrs, opt_options);
