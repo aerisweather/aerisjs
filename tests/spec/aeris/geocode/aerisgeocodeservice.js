65109c0992c11810cea1751aa45a92c294258eaa
Edan Schwartz <eschwartz@weathernation.net>
2014-07-01 14:15:40 -0500

    Mock /places endpoint response
    - pulled from the AerisGeocodeService spec

--- tests/spec/aeris/geocode/aerisgeocodeservice.js
+++ tests/spec/aeris/geocode/aerisgeocodeservice.js
@@ -1,50 +1,18 @@
 define([
   'aeris/util',
-  'mocks/aeris/config',
+  'mocks/api.aeris.com/places',
   'aeris/geocode/geocodeservicestatus',
   'aeris/geocode/aerisgeocodeservice',
   'mocks/aeris/api/models/aerisapimodel',
   'aeris/promise'
-], function(_, MockConfig, GeocodeServiceStatus, AerisGeocodeService, MockAerisApiModel, Promise) {
-
-  function PlacesSuccessResponse() {
-    return {
-      'success': true,
-      'error': null,
-      'response': {
-        'loc': {
-          'lat': 44.97997,
-          'long': -93.26384
-        },
-        'place': {
-          'name': 'Minneapolis',
-          'state': 'MN',
-          'stateFull': 'Minnesota',
-          'country': 'US',
-          'countryFull': 'United States',
-          'region': 'usnc',
-          'regionFull': 'North Central',
-          'continent': 'nam',
-          'continentFull': 'North America'
-        },
-        'profile': {
-          'elevM': 253,
-          'elevFT': 830,
-          'pop': 382578,
-          'tz': 'America\/Chicago',
-          'tzname': 'CST',
-          'tzoffset': -21600,
-          'isDST': false
-        }
-      }
-    };
-  }
+], function(_, PlacesSuccessResponse, GeocodeServiceStatus, AerisGeocodeService, MockAerisApiModel, Promise) {
 
   function PlacesApiResponseError(opt_code, opt_message) {
     var code = opt_code || 'invalid_location';
     var message = opt_message || 'MESSAGE_STUB';
 
     return {
+      name: 'APIResponseError',
       code: code,
       message: message
     };
