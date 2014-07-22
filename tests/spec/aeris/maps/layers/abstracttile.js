5ec974496231bf3a199df05a1a35166fee2a9f39
Edan Schwartz <eschwartz@weathernation.net>
2014-07-22 15:40:12 -0500

    Tile#preload

--- tests/spec/aeris/maps/layers/abstracttile.js
+++ tests/spec/aeris/maps/layers/abstracttile.js
@@ -9,8 +9,12 @@ define([
       name: 'STUB_NAME',
       server: 'STUB_SERVER'
     });
+    var options = _.defaults(opt_options || {}, {
+      strategy: function() {
+      }
+    });
 
-    AbstractTile.call(this, attrs, opt_options);
+    AbstractTile.call(this, attrs, options);
   };
   _.inherits(ConcreteTile, AbstractTile);
 
@@ -22,7 +26,8 @@ define([
   function testFactory() {
     var Tile = function() {
       var options = {
-        strategy: function() {}
+        strategy: function() {
+        }
       };
       var attrs = {
         name: 'myName',
@@ -40,7 +45,7 @@ define([
   }
 
 
-  describe('An AbstractTile', function() {
+  describe('AbstractTile', function() {
     describe('constructor', function() {
 
       it('should require a name', function() {
@@ -91,5 +96,76 @@ define([
         expect(test.tile.zoomFactor(zoom)).toEqual(zoom);
       });
     });
+
+
+    describe('preload', function() {
+      var tile, onResolve, onReject, mockMap;
+
+      beforeEach(function() {
+        tile = new ConcreteTile();
+        mockMap = { STUB: 'MAP' };
+        onResolve = jasmine.createSpy('onResolve');
+        onReject = jasmine.createSpy('onReject');
+      });
+
+      function loadTile(tile) {
+        tile.trigger('load');
+      };
+
+
+      it('should resolve immediately if the layer is already loaded', function() {
+        loadTile(tile);
+
+        tile.preload(mockMap).
+          done(onResolve).
+          fail(_.throwError);
+
+        expect(onResolve).toHaveBeenCalled();
+      });
+
+      it('should resolve once the layer has loaded', function() {
+        tile.preload(mockMap).
+          done(onResolve).
+          fail(_.throwError);
+        expect(onResolve).not.toHaveBeenCalled();
+
+        loadTile(tile);
+
+        expect(onResolve).toHaveBeenCalled();
+      });
+
+      it('should not change the layer\'s original attributes', function() {
+        var attrs_orig = {
+          opacity: 0.123,
+          map: { STUB: 'MAP_ORIG' }
+        };
+        tile.set(_.clone(attrs_orig));
+
+        tile.preload(mockMap);
+
+        expect(tile.pick(['opacity', 'map'])).toEqual(attrs_orig);
+      });
+
+      it('should reject if no map is provided', function() {
+        tile.preload().
+          fail(onReject);
+
+        expect(onReject).toHaveBeenCalled();
+      });
+
+      it('should set the layer to the provided map', function() {
+        var onChangeMap = jasmine.createSpy('onChangeMap');
+        tile.once('change:map', onChangeMap);
+
+        onChangeMap.andCallFake(function() {
+          expect(tile.get('map')).toEqual(mockMap);
+        });
+
+        tile.preload(mockMap);
+        expect(onChangeMap).toHaveBeenCalled();
+      });
+
+    });
+
   });
 });
