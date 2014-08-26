b6c739d420a0aa95d440af1a8b7c5210ff7529db
Edan Schwartz <eschwartz@weathernation.net>
2014-08-26 17:23:25 -0500

    Fix mapbox-amd module
    - Use correct global variable name
    - Throw errors if dependencies are not defined.

--- src/maps/leaflet/mapbox-amd.js
+++ src/maps/leaflet/mapbox-amd.js
@@ -1,4 +1,12 @@
 define(['leaflet'], function(L) {
-  return L.mapBox;
+  if (!L) {
+    throw new Error('Aeris.js requires Leaflet. See http://leafletjs.com/examples/quick-start.html.');
+  }
+  if (!L.mapbox) {
+    throw new Error('Aeris.js requires MapBox.js in order to use aeris.maps.layers.MapBox layers. ' +
+      'See https://www.mapbox.com/mapbox.js.');
+  }
+
+  return L.mapbox;
 });
 
