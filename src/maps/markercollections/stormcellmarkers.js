63d1ea6d9bc0a9f72dbae97e20ec68d1fe1f7f8f
Edan Schwartz <eschwartz@weathernation.net>
2015-05-29 13:51:02 -0500

    StormCells: Individual markes use Leaflet strategy
    instead of MarkerCollection
    
    - I think this will be more performant,
      as the view will not have to entirely
      redraw on every update

--- src/maps/markercollections/stormcellmarkers.js
+++ src/maps/markercollections/stormcellmarkers.js
@@ -17,14 +17,12 @@ define([
         }
       }),
       model: StormCellMarker,
-      strategy: StormCellsStrategy,
+      strategy: _.noop,
       // StormCells do not yet support clustering
       clusterStrategy: null,
-      cluster: false,
-      style: this.getStyleDefault_.bind(this)
+      cluster: false
     });
 
-    this.getStyle = _.isFunction(options.style) ? options.style : _.constant(options.style);
 
     PointDataMarkers.call(this, opt_models, options);
   };
@@ -33,67 +31,6 @@ define([
 
   StormCellMarkers.prototype.toGeoJson = function() {
     return this.data_.toGeoJson();
-  };
-
-  StormCellMarkers.prototype.getStyleDefault_ = function(properties) {
-    var styles = {
-      cell: {
-        radius: 4,
-        fillColor: 'green',
-        color: '#000',
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8,
-        hover: {
-          radius: 7,
-          fillOpacity: 1
-        }
-      },
-      cone: {
-        stroke: true,
-        color: '#030303',
-        weight: 1,
-        opacity: 0.8,
-        fillColor: '#f66',
-        fillOpacity: 0.2,
-        hover: {
-          weight: 2,
-          fillOpacity: 0.8,
-          fillColor: '#f66'
-        }
-      },
-      line: {
-        stroke: true,
-        color: '#030303',
-        weight: 1,
-        opacity: 0.8,
-        hover: {}
-      }
-    };
-
-    // Tornado
-    if (properties.ob.tvs > 0 || properties.ob.mda > 10) {
-      _.extend(styles.cell, {
-        radius: 5,
-        fillColor: 'red'
-      });
-    }
-    // Hail
-    else if (properties.ob.hail && properties.ob.hail.prob >= 70) {
-      _.extend(styles.cell, {
-        radius: 5,
-        fillColor: 'yellow'
-      });
-    }
-    // rotating
-    else if (properties.ob.mda > 0) {
-      _.extend(styles.cell, {
-        radius: 5,
-        fillColor: 'orange'
-      });
-    }
-
-    return styles;
   };
 
   StormCellMarkers.prototype.startClustering = function() {
--- src/maps/markers/stormcellmarker.js
+++ src/maps/markers/stormcellmarker.js
@@ -1,18 +1,86 @@
 define([
   'aeris/util',
-  'aeris/maps/extensions/mapextensionobject'
-], function(_, MapExtensionObject) {
+  'aeris/maps/extensions/mapextensionobject',
+  'aeris/maps/strategy/markers/stormcells'
+], function(_, MapExtensionObject, StormCellStrategy) {
   /** @class StormCellMarker */
   var StormCellMarker = function(opt_attrs, opt_options) {
     var options = _.defaults(opt_options || {}, {
-      // StormCellMarkers may currently only be
-      // rendered as a MarkerCollection (not as individual markers)
-      strategy: _.noop
+      strategy: StormCellStrategy,
+      style: this.getStyleDefault_.bind(this)
     });
+
+    this.getStyle = _.isFunction(options.style) ? options.style : _.constant(options.style);
+
     MapExtensionObject.call(this, opt_attrs, options);
   };
   _.inherits(StormCellMarker, MapExtensionObject);
 
+
+  StormCellMarker.prototype.toGeoJson = function() {
+    return this.getData().toJSON();
+  };
+
+  StormCellMarker.prototype.getStyleDefault_ = function(properties) {
+    var styles = {
+      cell: {
+        radius: 4,
+        fillColor: '#3bca24',
+        color: '#000',
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8,
+        hover: {
+          radius: 7,
+          fillOpacity: 1
+        }
+      },
+      cone: {
+        stroke: true,
+        color: '#030303',
+        weight: 1,
+        opacity: 0.8,
+        fillColor: '#f66',
+        fillOpacity: 0.2,
+        hover: {
+          weight: 2,
+          fillOpacity: 0.8,
+          fillColor: '#f66'
+        }
+      },
+      line: {
+        stroke: true,
+        color: '#030303',
+        weight: 1,
+        opacity: 0.8,
+        hover: {}
+      }
+    };
+
+    // Tornado
+    if (properties.ob.tvs > 0 || properties.ob.mda > 10) {
+      _.extend(styles.cell, {
+        radius: 5,
+        fillColor: '#fc0d1b'
+      });
+    }
+    // Hail
+    else if (properties.ob.hail && properties.ob.hail.prob >= 70) {
+      _.extend(styles.cell, {
+        radius: 5,
+        fillColor: '#2dfffe'
+      });
+    }
+    // rotating
+    else if (properties.ob.mda > 0) {
+      _.extend(styles.cell, {
+        radius: 5,
+        fillColor: '#fecb2f'
+      });
+    }
+
+    return styles;
+  };
 
   return StormCellMarker;
 });
